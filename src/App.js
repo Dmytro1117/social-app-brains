88f8ca74563d97a1786d1ddfe34b915b69cec239
Dmytro <111579384+Dmytro1117@users.noreply.github.com>
2023-05-04 22:12:51 +0300

    dfgfd

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Dialog from "./page/Dialog";
 // Генеруємо наш додаток, який потім у файлі index.js ми будемо виводити в браузері
 export default function App() {
   return (
-    <BrowserRouter basename="/social-app-brains/chats">
+    <BrowserRouter basename="/social-app-brains">
       <Routes>
         {/* Підключаємо сторінку Чати за посиланням /chats */}
         <Route path="/chats" element={<Chats />} />
